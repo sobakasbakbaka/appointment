7910ce170281b74d2959f13862283aaf24b4069c
Nikita <108035712+sobakasbakbaka@users.noreply.github.com>
2024-11-17 22:50:40 +0500

    fix cors

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,11 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: '*',
+    methods: 'GET,HEAD,PUT,POST,DELETE,OPTIONS',
+    allowedHeaders: ['Content-Type, Authorization'],
+  });
   await app.listen(3000);
 }
 bootstrap();
