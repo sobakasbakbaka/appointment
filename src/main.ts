7297efa17e37e3f76644dc02207553ab15cbe571
Nikita <108035712+sobakasbakbaka@users.noreply.github.com>
2024-11-24 22:24:38 +0500

    added global prefix

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ import * as cookieParser from 'cookie-parser';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.use(cookieParser());
-
+  app.setGlobalPrefix('api');
   app.enableCors({
     origin: process.env.CORS_ORIGIN,
     methods: 'GET,HEAD,PUT,POST,DELETE,OPTIONS',
